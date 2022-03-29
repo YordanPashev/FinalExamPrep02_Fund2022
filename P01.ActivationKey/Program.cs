fb6e82dc85e6eb7205a2dc0d6ce6f277f428b6e7
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-03-29 22:48:33 +0300

    Adding Problem Description to Problem 1 - ActivationKey

--- P01.ActivationKey/Program.cs
+++ P01.ActivationKey/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P01.ActivationKey
 {
@@ -6,7 +7,84 @@ namespace P01.ActivationKey
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string rawActKey = Console.ReadLine();
+            string cmd;
+
+            while ((cmd = Console.ReadLine()) != "Generate")
+            {
+                string[] cmdArgs = cmd
+                    .Split(">>>", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string action = cmdArgs[0];
+
+                if (action == "Contains")
+                {
+                    string substringToFind = cmdArgs[1];
+                    rawActKey = CheckIfKeyContainsThisSubstring(rawActKey, substringToFind);
+                }
+
+                else if (action == "Flip")
+                {
+                    rawActKey = ChangesTheSubstringBetweenTheGivenIndices(rawActKey, cmdArgs);
+                    Console.WriteLine(rawActKey);
+                }
+
+                else if (action == "Slice")
+                {
+                    rawActKey = DeletesTheCharactersBetweenTheGivenIndices(rawActKey, cmdArgs);
+                    Console.WriteLine(rawActKey);
+                }
+            }
+
+            Console.WriteLine($"Your activation key is: {rawActKey}");
+        }
+
+        static string CheckIfKeyContainsThisSubstring(string rawActKey, string substring)
+        {
+            if (rawActKey.Contains(substring))
+            {
+                Console.WriteLine($"{rawActKey} contains {substring}");
+            }
+
+            else
+            {
+                Console.WriteLine("Substring not found!");
+            }
+
+            return rawActKey;
+        }
+
+        static string ChangesTheSubstringBetweenTheGivenIndices(string rawActKey, string[] cmdArgs)
+        {
+            string flipArgs = cmdArgs[1];
+            int startIndex = int.Parse(cmdArgs[2]);
+            int endIndex = int.Parse(cmdArgs[3]);
+            int charNumberToChange = endIndex - startIndex;
+
+            if (flipArgs == "Upper")
+            {
+                string substringToFLip = rawActKey.Substring(startIndex, charNumberToChange);
+                rawActKey = rawActKey.Replace(substringToFLip, substringToFLip.ToUpper());
+            }
+
+            else if (flipArgs == "Lower")
+            {
+                string substringToFLip = rawActKey.Substring(startIndex, charNumberToChange);
+                rawActKey = rawActKey.Replace(substringToFLip, substringToFLip.ToLower());
+            }
+
+            return rawActKey;
+        }
+
+        static string DeletesTheCharactersBetweenTheGivenIndices(string rawActKey, string[] cmdArgs)
+        {
+            int startIndex = int.Parse(cmdArgs[1]);
+            int endIndex = int.Parse(cmdArgs[2]);
+            int charNumberToDelete = endIndex - startIndex;
+
+            rawActKey = rawActKey.Remove(startIndex, charNumberToDelete);
+
+            return rawActKey;
         }
     }
 }
