4923e76ef990dfd0b8020d479651b1098099ab0f
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-02 12:07:38 +0300

    Adding Solution to new Problem 1 nad 2 - ImitationGame and AddAstra

---
+++ FinalExamPrep/P01.ImitationGame/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.ImitationGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Decode")
+            {
+                string[] cmdArgs = cmd
+                    .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string action = cmdArgs[0];
+
+                if (action == "Move")
+                {
+                    message = MoveFirstLetterToTheBack(message, cmdArgs);
+                }
+
+                else if (action == "Insert")
+                {
+                    message = InsertGivenValueOnIndex(message, cmdArgs);
+                }
+
+                else if (action == "ChangeAll")
+                {
+                    message = ReplaceGivenSubstring(message, cmdArgs);
+                }
+            }
+
+            Console.WriteLine($"The decrypted message is: {message}");
+        }
+
+        static string MoveFirstLetterToTheBack(string messgae, string[] cmdArgs)
+        {
+            int numberOfLetter = int.Parse(cmdArgs[1]);
+            string sbstringToMove = messgae.Substring(0, numberOfLetter);
+            messgae = messgae.Remove(0, numberOfLetter);
+
+            return messgae + sbstringToMove;
+
+        }
+
+        static string InsertGivenValueOnIndex(string messgae, string[] cmdArgs)
+        {
+            int index = int.Parse(cmdArgs[1]);
+            string value = cmdArgs[2];
+
+            return messgae.Insert(index, value);
+        }
+
+        static string ReplaceGivenSubstring(string messgae, string[] cmdArgs)
+        {
+            string oldSubstring = cmdArgs[1];
+            string newSubstring = cmdArgs[2];
+
+            return messgae.Replace(oldSubstring, newSubstring);
+        }
+    }
+}
---
+++ FinalExamPrep/P02.AddAstra/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P02.AddAstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int totalCalories = 0;
+            Regex regex = new Regex(@"([#|\|])(?<item>[A-Za-z\s]+)(\1)(?<date>[0-9]{2}\/[0-9]{2}\/[0-9]{2})(\1)(?<kcal>[0-9]+)(\1)");
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match item in matches)
+            {
+                totalCalories += int.Parse(item.Groups["kcal"].Value);
+            }
+
+            int days = totalCalories / 2000;
+
+            Console.WriteLine($"You have food to last you for: {days} days!");
+
+            foreach (Match item in matches)
+            {
+                Console.WriteLine($"Item: {item.Groups["item"].Value}, Best before: {item.Groups["date"].Value}, Nutrition: {item.Groups["kcal"].Value}");
+            }
+        }
+    }
+}
