ce7fa1c2641f02acc16dd748cb7fc9774d696f17
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-04 19:23:06 +0300

    Adding Solutions to P08 - P013

---
+++ BasycSyntax/BasicSyntax-Lab/P09.SumOfOddNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P09.SumOfOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0, i = 1;
+            bool isFinish = false;
+
+            while (isFinish == false)
+            {
+                if (n == 0)
+                {
+                    isFinish = true;
+                    break;
+                }
+                sum += i;
+                Console.WriteLine(i);
+                i += 2;
+                n--;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ BasycSyntax/BasicSyntax-Lab/P11.MultiplicationTable2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P11.MultiplicationTable2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var times = int.Parse(Console.ReadLine());
+
+            for (int i = times; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} X {i} = {number * i}");
+            }
+
+            if (times > 10)
+            {
+                Console.WriteLine($"{number} X {times} = {number * times}");
+            }
+        }
+    }
+}
