4e4c264398119fbe93a8eb88de41384f4527bbba
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-08 23:09:35 +0300

    Relocating Foler DT-Lab and Adding Problems Solution to P06 and P08

---
+++ DataTypesAndVariables/DataTypesAndVariables-Exercise/P06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P06.TriplesOfLatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            char end = (char)(count + 96);
+            for (char i = 'a'; i <= end; i++)
+            {
+                for (char j = 'a'; j <= end; j++)
+                {
+                    for (char k = 'a'; k <= end; k++)
+                    {
+                        Console.WriteLine($"{i}{j}{k}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DataTypesAndVariables/DataTypesAndVariables-Exercise/P07.WaterOverflow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P07.WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int courses = int.Parse(Console.ReadLine());
+            int littersInTheTank = 0;
+            for (int i = 1; i <= courses; i++)
+            {
+                int course = int.Parse(Console.ReadLine());
+                if (littersInTheTank + course > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+                else
+                {
+                    littersInTheTank += course;
+                }
+            }
+            Console.WriteLine(littersInTheTank);
+        }
+    }
+}
