d7428ca676005a7b5cce169d831cbebef3174614
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-09 23:22:38 +0300

    Adding Problems Descriptions and Solution to P01

---
+++ DataTypesAndVariables/DataTypesAndVariables-MoreExercise/P01.DataTypeFinder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace P01.DataTypeFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            int valueInt;
+            char valueChar;
+            bool valueBool;
+            float valueFloat;
+            while (input != "END")
+            {
+                if (int.TryParse(input, out valueInt))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (float.TryParse(input, out valueFloat))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (bool.TryParse(input, out valueBool))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else if (char.TryParse(input, out valueChar))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
