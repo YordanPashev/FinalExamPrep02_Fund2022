af4b438d50c04ee1e3fe0f7ec0245bf177027fc4
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-20 22:38:45 +0300

    Adding Arrays-MoreExercise Problems Description and Solution to P01, P02, P04 and P05

---
+++ Arrays/Array-MoreExercise/P01.Encrypt,SortAndPrintArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace P01.Encrypt_SortAndPrintArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int[] scores = new int[lines];
+
+            for (int i = 0; i < lines; i++)
+            {
+                string letter = Console.ReadLine();
+
+                int vowelsSum = 0;
+                int consonantSum = 0;
+
+                for (int j = 0; j < letter.Length; j++)
+                {
+                    if (letter[j] == 'a' || letter[j] == 'e' ||
+                        letter[j] == 'i' || letter[j] == 'o' || letter[j] == 'u' ||
+                        letter[j] == 'A' || letter[j] == 'E' ||
+                        letter[j] == 'I' || letter[j] == 'O' || letter[j] == 'U')
+                    {
+                        vowelsSum += (int)letter[j] * letter.Length;
+                    }
+                    else
+                    {
+                        consonantSum += (int)letter[j] / letter.Length;
+                    }
+                }
+                scores[i] = consonantSum + vowelsSum;
+            }
+
+            Array.Sort(scores);
+
+            foreach (int result in scores)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
