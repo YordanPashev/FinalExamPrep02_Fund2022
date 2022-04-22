7b8f6923349f8061f81bc664d3e2db69413e092b
YordanPashev <92952122+YordanPashev@users.noreply.github.com>
2022-04-22 22:51:45 +0300

    Add files via upload
    Adding Problems Solution from P03 to P08

---
+++ Methods/Methods-Exercise/P05.AddAndSubtract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P05.AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstDigit = int.Parse(Console.ReadLine());
+            int secondDigit = int.Parse(Console.ReadLine());
+            int thirdDigit = int.Parse(Console.ReadLine());
+            int addResult = Add(firstDigit, secondDigit);
+            int substractResult = Substract(addResult, thirdDigit);
+            Console.WriteLine(substractResult);
+        }
+
+        static int Add(int firstDigit, int secondDigit)
+        {
+            return firstDigit + secondDigit;
+        }
+
+        static int Substract(int addSum, int thirdDigit)
+        {
+            return addSum - thirdDigit;
+        }
+    }
+}
