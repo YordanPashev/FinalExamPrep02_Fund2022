abf53b573f52cd4bfb251caab930d89fe71270ec
YordanPashev <92952122+YordanPashev@users.noreply.github.com>
2022-04-24 23:05:49 +0300

    Adding Solution to P03, P04 and P05

---
+++ Methods/Methods-MoreExercise/P03.LongerLine/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace P03.LongerLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+
+            FindLongestLine(x1, y1, x2, y2, x3, y3, x4, y4);
+        }
+
+        static void FindLongestLine(double x1, double y1, double x2, double y2,
+                                    double x3, double y3, double x4, double y4)
+        {
+            double lineOneLenght = Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2);
+            double lineTwolinght = Math.Pow(x3 - x4, 2) + Math.Pow(y3 - y4, 2);
+
+            if (lineOneLenght >= lineTwolinght)
+            {
+                PrintClosestPointToOrigin(x1, y1, x2, y2);
+            }
+
+            else
+            {
+                PrintClosestPointToOrigin(x3, y3, x4, y4);
+            }
+        }
+
+        static void PrintClosestPointToOrigin(double x, double y, double x1, double y1)
+        {
+            if ((x * x) + (y * y) <= (x1 * x1) + (y1 * y1))
+            {
+                Console.WriteLine($"({x}, {y})({x1}, {y1})");
+            }
+
+            else
+            {
+                Console.WriteLine($"({x1}, {y1})({x}, {y})");
+            }
+        }
+    }
+}
---
+++ Methods/Methods-MoreExercise/P05.MultiplicationSign/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace P05.MultiplicationSign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int[] arrOfTheThreeNumbers = new int[3] { firstNumber, secondNumber, thirdNumber };
+
+            if (arrOfTheThreeNumbers.Contains(0))
+            {
+                Console.WriteLine("zero");
+            }
+            else
+            {
+                string result = CheckResultNegativeOrPositive(arrOfTheThreeNumbers);
+                Console.WriteLine(result);
+            }
+        }
+
+        static string CheckResultNegativeOrPositive(int[] arrOfTheThreeNumbers)
+        {
+            int negativeNumbersCounter = 0;
+            string result = string.Empty;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (arrOfTheThreeNumbers[i] < 0)
+                {
+                    negativeNumbersCounter++;
+                }
+            }
+
+            if (negativeNumbersCounter == 0 || negativeNumbersCounter == 2)
+            {
+                result = "positive";
+            }
+            else if (negativeNumbersCounter == 1 || negativeNumbersCounter == 3)
+            {
+                result = "negative";
+            }
+
+            return result;
+        }
+    }
+}
