5e1f5f815de1cb550b591fcf2dc4ac5a1a510354
YordanPashev <92952122+YordanPashev@users.noreply.github.com>
2022-04-24 22:59:12 +0300

    Adding All Problems Description
    And also Problem Solution to P01 and P02

---
+++ Methods/Methods-MoreExercise/P01DataTypes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace P01DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string inputType = Console.ReadLine();
+                string input = Console.ReadLine();
+
+                if (inputType == "int")
+                {
+                    int result = MyltiplyByTwo(int.Parse(input));
+                    Console.WriteLine(result);
+                }
+
+                else if (inputType == "real")
+                {
+                    double result = MyltiplyByOneAndAHalf(double.Parse(input));
+                    Console.WriteLine($"{result:F2}");
+                }
+
+                else if (inputType == "string")
+                {
+                    PrintString(input);
+                }
+            }
+
+            static int MyltiplyByTwo(int number)
+            {
+                int result = number * 2;
+                return result;
+            }
+
+            static double MyltiplyByOneAndAHalf(double number)
+            {
+                double result = number * 1.5;
+                return result;
+            }
+
+            static void PrintString(string input)
+            {
+                Console.WriteLine($"${input}$");
+            }
+        }
+    }
+}
