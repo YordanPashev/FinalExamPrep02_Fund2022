6a259d6cf84041fe0b80125e6492a95cb730ec10
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-26 22:35:55 +0300

    Adding Solution to P07, P08 and P09

---
+++ Methods/Methoods-Lab/P07.RepeatString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace P07.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                string input = Console.ReadLine();
+                int repeatTimes = int.Parse(Console.ReadLine());
+                Console.WriteLine(GetStringResult(input, repeatTimes));
+            }
+            static string GetStringResult(string input, int rep)
+            {
+                StringBuilder result = new StringBuilder();
+                for (int i = 0; i < rep; i++)
+                {
+                    result.Append(input);
+                }
+                return result.ToString();
+            }
+
+        }
+    }
+}
---
+++ Methods/Methoods-Lab/P08.MathPower/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double digit = double.Parse(Console.ReadLine());
+            double powerNum = double.Parse(Console.ReadLine());
+            Console.WriteLine(MathPower(digit, powerNum));
+        }
+        static double MathPower(double digit, double power)
+        {
+            double result = 1;
+            for (double i = 0; i < power; i++)
+            {
+                result *= digit;
+            }
+            return result;
+        }
+    }
+}
