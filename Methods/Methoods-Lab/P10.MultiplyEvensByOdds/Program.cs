d86da6e7b9c4f44863a8dbe607455d94340f9224
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-04-26 22:38:14 +0300

    Adding Solution to P10 and P11

---
+++ Methods/Methoods-Lab/P10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P10.MultiplyEvensByOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int resultOfEvenSum = GetSumOfEvenDigits(input);
+            int resultOfOddSum = GetSumOfOddDigits(input);
+            int result = resultOfEvenSum * resultOfOddSum;
+            Console.WriteLine(result);
+        }
+        static int GetSumOfEvenDigits(int digit)
+        {
+            int evenSum = 0;
+            while (digit != 0)
+            {
+                int currentDigit = Math.Abs(digit) % 10;
+                if (currentDigit % 2 == 0)
+                {
+                    evenSum += currentDigit;
+                }
+                digit /= 10;
+            }
+            return evenSum;
+        }
+
+        static int GetSumOfOddDigits(int digit)
+        {
+            int oddSum = 0;
+            while (digit != 0)
+            {
+                int currentDigit = Math.Abs(digit) % 10;
+                if (currentDigit % 2 != 0)
+                {
+                    oddSum += currentDigit;
+                }
+                digit /= 10;
+            }
+            return oddSum;
+        }
+    }
+}
---
+++ Methods/Methoods-Lab/P11.MathOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P11.MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            char @operator = char.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+            Console.WriteLine(Calculate(firstNum, secondNum, @operator));
+        }
+        static double Calculate(double firstNum, double secondNum, char @operator)
+        {
+            double result = 0;
+            switch (@operator)
+            {
+                case '+':
+                    result = firstNum + secondNum;
+                    break;
+                case '-':
+                    result = firstNum - secondNum;
+                    break;
+                case '*':
+                    result = firstNum * secondNum;
+                    break;
+                case '/':
+                    result = firstNum / secondNum;
+                    break;
+            }
+            return result;
+        }
+    }
+}
+
