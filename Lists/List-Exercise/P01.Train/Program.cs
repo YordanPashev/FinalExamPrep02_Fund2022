9291ea622bb80e48eeeec49f3465e97b88c1a433
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-05-24 15:15:25 +0300

    Adding Solution to P01 and P02

---
+++ Lists/List-Exercise/P01.Train/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOFWagons = Console.ReadLine()
+                                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                     .Select(int.Parse)
+                                     .ToList();
+
+            int maxCapacityPerWagon = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] cmdArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                if (cmdArgs[0] == "Add")
+                {
+                    int numberOFPassengersInTheNewWagon = int.Parse(cmdArgs[1]);
+                    listOFWagons.Add(numberOFPassengersInTheNewWagon);
+                }
+
+                else
+                {
+                    int numberOFPassengersToAddInExistingWagon = int.Parse(cmdArgs[0]);
+
+                    for (int i = 0; i < listOFWagons.Count; i++)
+                    {
+
+                        if (listOFWagons[i] + numberOFPassengersToAddInExistingWagon <= maxCapacityPerWagon)
+                        {
+                            listOFWagons[i] += numberOFPassengersToAddInExistingWagon;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", listOFWagons));
+        }
+    }
+}
---
+++ Lists/List-Exercise/P02.ChangeList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOFWagons = Console.ReadLine()
+                                      .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(int.Parse)
+                                      .ToList();
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] cmdArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string action = cmdArgs[0];
+
+                if (action == "Delete")
+                {
+                    int elementToDelete = int.Parse(cmdArgs[1]);
+                    listOFWagons.RemoveAll(element => elementToDelete == element);
+                }
+
+                else if (action == "Insert")
+                {
+                    int elementToAdd = int.Parse(cmdArgs[1]);
+                    int index = int.Parse(cmdArgs[2]);
+
+                    listOFWagons.Insert(index, elementToAdd);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", listOFWagons));
+        }
+    }
+}
