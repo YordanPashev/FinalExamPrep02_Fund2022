272fa6ab5af1733366dccb8ee7ec56e608845157
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-04 20:05:51 +0300

    Adding Solution to P03 and 04

---
+++ ObjectsAndClasses/ObjectsAndClasses-Lab/P03.Songs/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.Songs
+{
+    class Song
+    {
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int numberOfSongs = int.Parse(Console.ReadLine());
+            List<Song> listOfSongs = new List<Song>();
+
+            for (int i = 0; i < numberOfSongs; i++)
+            {
+                List<string> input = Console.ReadLine()
+                    .Split('_')
+                    .ToList();
+
+                Song currentSong = new Song()
+                {
+                    TypeList = input[0],
+                    Name = input[1],
+                    Time = input[2]
+                };
+
+                listOfSongs.Add(currentSong);
+            }
+
+
+            string printCmd = Console.ReadLine();
+
+            if (printCmd == "all")
+            {
+                foreach (var song in listOfSongs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+
+            else
+            {
+                string typeListTosearch = printCmd;
+
+                List<Song> filteredSong = listOfSongs.FindAll(song => song.TypeList == typeListTosearch).ToList();
+
+                foreach (var song in filteredSong)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ ObjectsAndClasses/ObjectsAndClasses-Lab/P04.Students/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.Students
+{
+    class Students
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+
+
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Students> listOfStudents = new List<Students>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] data = input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string studentsFirstName = data[0];
+                string studentsLastName = data[1];
+                int studentsAge = int.Parse(data[2]);
+                string homeTownName = data[3];
+
+                Students currentStudenInformation = new Students()
+                {
+                    FirstName = studentsFirstName,
+                    LastName = studentsLastName,
+                    Age = studentsAge,
+                    HomeTown = homeTownName
+                };
+
+                listOfStudents.Add(currentStudenInformation);
+            }
+
+            string nameOfTowToSearch = Console.ReadLine();
+
+            List<Students> filteredStudentsByPlace = listOfStudents.FindAll(s => s.HomeTown == nameOfTowToSearch);
+
+            foreach (Students student in filteredStudentsByPlace)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+}
