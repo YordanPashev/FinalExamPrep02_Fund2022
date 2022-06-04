34d418649786f96dd375234585bb117cde2602d7
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-04 20:03:39 +0300

    Adding All Problems Descriptions and Solutions to P01 and 02

---
+++ ObjectsAndClasses/ObjectsAndClasses-Lab/P01.RandomizeWords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfWords = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Random random = new Random();
+
+            for (int i = 0; i < arrayOfWords.Length; i++)
+            {
+                int randomIndex = random.Next(0, arrayOfWords.Length);
+                string currentWord = arrayOfWords[i];
+                arrayOfWords[i] = arrayOfWords[randomIndex];
+                arrayOfWords[randomIndex] = currentWord;
+            }
+
+            foreach (var word in arrayOfWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ ObjectsAndClasses/ObjectsAndClasses-Lab/P02.BigFactorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace P02.BigFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            BigInteger factorialResult = 1;
+
+            for (int i = number; i >= 2; i--)
+            {
+                factorialResult *= i;
+            }
+
+            Console.WriteLine(factorialResult);
+        }
+    }
+}
