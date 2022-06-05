7116bb680a3cfe62b9562125ef189ea66c0f4342
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-05 23:54:03 +0300

    Adding Solutions to P04 and 05

---
+++ Lists/Lists-MoreExercise/P04.MixedUpLists/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.MixedUpLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOne = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> listTwo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] range = GetRangeValuesAndRemoveThemFromTheList(listOne, listTwo);
+            Array.Sort(range);
+
+            int minIndexVAlue = range[0];
+            int maxIndexVAlue = range[1];
+
+            List<int> result = MixUpTwoLists(listOne, listTwo);
+            result.RemoveAll(x => x >= maxIndexVAlue || x <= minIndexVAlue);
+
+            result.Sort();
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        static int[] GetRangeValuesAndRemoveThemFromTheList(List<int> listOne, List<int> listTwo)
+        {
+            if (listOne.Count > listTwo.Count)
+            {
+                int[] rangeValues = new int[2]
+                {
+                  listOne[listOne.Count - 1],
+                  listOne[listOne.Count - 2]
+                };
+                listOne.RemoveRange(listOne.Count - 2, 2);
+
+                return rangeValues;
+            }
+
+            else
+            {
+                int[] rangeValues = new int[2]
+                {
+                  listTwo[0],
+                  listTwo[1]
+                };
+
+                listTwo.RemoveRange(0, 2);
+
+                return rangeValues;
+            }
+        }
+
+        static List<int> MixUpTwoLists(List<int> listOne, List<int> listTwo)
+        {
+            listTwo.Reverse();
+            int secondListIndex = 0;
+            List<int> result = listOne;
+
+            for (int firstListIndex = 0; firstListIndex <= listOne.Count; firstListIndex++)
+            {
+                if (firstListIndex % 2 != 0)
+                {
+                    result.Insert(firstListIndex, listTwo[secondListIndex]);
+                    secondListIndex++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Lists/Lists-MoreExercise/P05.DrumSet/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.DrumSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+
+            List<int> drumSet = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] initialQualityValues = drumSet.ToArray();
+            List<int> initialQualityList = initialQualityValues.ToList();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int hitPower = int.Parse(input);
+
+                DecreassTheValueOfTheDrumsQuality(drumSet, hitPower);
+
+                if (drumSet.Any(x => x <= 0))
+                {
+                    for (int i = 0; i < drumSet.Count; i++)
+                    {
+                        if (drumSet[i] <= 0)
+                        {
+                            if (BuyANewDrum(i, budget, initialQualityList))
+                            {
+                                budget -= initialQualityList[i] * 3;
+                                drumSet[i] = initialQualityList[i];
+                            }
+
+                            else
+                            {
+                                drumSet.RemoveAt(i);
+                                initialQualityList.RemoveAt(i);
+                                i--;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", drumSet));
+            Console.WriteLine($"Gabsy has {budget:F2}lv.");
+        }
+
+        static void DecreassTheValueOfTheDrumsQuality(List<int> drumSet, int hotPower)
+        {
+            for (int i = 0; i < drumSet.Count; i++)
+            {
+                drumSet[i] -= hotPower;
+            }
+        }
+
+        static bool BuyANewDrum(int drumSetIndex, decimal budget, List<int> initialQualityList)
+        {
+            if (initialQualityList[drumSetIndex] * 3 <= budget)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
